c55356dd214c2fef6d1ed44337f0ad11d61e0ed2
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2021-06-24 17:25:36 +0100

    Initial Commit

---
+++ TestProgressCircle/TestProgressCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProgressCircleApp.swift
+//  TestProgressCircle
+//
+//  Created by Nigel Gee on 24/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestProgressCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
