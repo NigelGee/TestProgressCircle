170b3abb20a7998344faa09a97d2283353fb46e7
Nigel Gee <41487836+NigelGee@users.noreply.github.com>
2021-06-24 17:37:03 +0100

    Intiial Commit

--- TestProgressCircle/ContentView.swift
+++ TestProgressCircle/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var progress: Double = 0.25
+
+    private var formattedPercent: String {
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 1
+        formatter.numberStyle = .percent
+        return formatter.string(from: NSNumber(value: progress)) ?? ""
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Circle()
+                .stroke(Color.black, lineWidth: 24)
+
+
+            Text(formattedPercent)
+                .font(.system(size: 64))
+
+            Circle()
+                .trim(from: 0.0, to: CGFloat(progress))
+                .stroke(Color.blue, style: StrokeStyle(lineWidth: 13, lineCap: .round))
+                .rotationEffect(.degrees(-90))
+        }
+        .padding()
+        .onTapGesture {
+            withAnimation {
+                progress = min(1.0, progress + 0.25)
+            }
+        }
     }
 }
 
